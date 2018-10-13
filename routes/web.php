8f1e3f71ac27e86d0d0bdbf78dbc7cd78a5456ea
candra saputra <candrasaputra@users.noreply.github.com>
2018-10-13 23:57:58 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
+
+// Overide rote register to login
+Route::match(["GET", "POST"], "/register", function(){
+    return redirect("/login");
+})->name("register");
 
 Route::get('/home', 'HomeController@index')->name('home');
